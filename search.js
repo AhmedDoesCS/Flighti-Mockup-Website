6c408701496c163af1a56e058fed434893e58c9d
AhmedSQRD <aahmed@aisct.org>
2024-10-05 03:05:37 +0200

    Worked on logic behind search-results generation.

--- search.js
+++ search.js
@@ -3,6 +3,9 @@ let searchItem;
 const searchBar = document.getElementById('searchBar');
 const searchButton = document.getElementById("searchButton");
 const clearButton = document.getElementById("clearButton");
+const results = document.getElementById("results");
+
+
 
 searchButton.addEventListener('click', () => {
     fetch("./travel_recommendation_api.json")
@@ -25,7 +28,17 @@ searchButton.addEventListener('click', () => {
 
         if (Object.keys(obj).includes(searchItem)) {
             obj[`${searchItem}`].forEach((item) => {
-                console.log(item);
+                const block = document.createElement("div");
+                block.style.cssText = "background-color: #ffff; width:100%; height: 50vh; border-radius: 20px;";
+                
+                const img = document.createElement("img");
+                img.setAttribute("src", `${item.imageUrl}`);
+                img.style.cssText = "height: 100%; width: 100%; border-radius: inherit;";
+
+                block.append(img);
+
+
+                results.append(block);
             });
         }
     })
